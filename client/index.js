5b23966676613ffc30148a40167ea12b9654ecf6
w3ia <90132132+w3ia@users.noreply.github.com>
2023-02-21 00:27:01 +0000

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,17 +1,29 @@
 const axios = require('axios');
+const prompt = require('prompt-sync')();
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
-async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+const merkleTree = new MerkleTree(niceList);
 
+async function checkList(name, proof) {
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name,
+    proof: proof
   });
 
-  console.log({ gift });
+  return gift;
+}
+
+async function main() {
+  const name = prompt('What is your name? ');
+  console.log(`Hey there ${name}, let's check if you are on the gift list...`);
+  
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+
+  console.log(await checkList(name, proof));
 }
 
 main();
