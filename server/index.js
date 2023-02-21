d3775389a43ab8e6b69bab4cc915b78d2d6fdd2f
w3ia <90132132+w3ia@users.noreply.github.com>
2023-02-21 00:27:50 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,18 +6,19 @@ const port = 1225;
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
+// hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+// create the merkle tree for the whole nice list
+const MERKLE_ROOT = "ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa";
 
 app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+  const name = req.body.name;
+  const proof = req.body.proof;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(proof, name , MERKLE_ROOT)
+  
   if(isInTheList) {
-    res.send("You got a toy robot!");
+    res.send("Yes! You are on the list. You got a toy robot!");
   }
   else {
     res.send("You are not on the list :(");
